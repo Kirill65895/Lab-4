5c583c298aace2341618ae3ea0414d96c628a3e1
Kirill65895 <kurasovkirill06@gmail.com>
2024-10-16 01:48:25 +0700

    Add files via upload

--- Laba 5.cpp
+++ Laba 5.cpp
@@ -1,50 +1,28 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <string>
 using namespace std;
 
-int sumValuesArreySmallerVariable() 
+int sumValuesArreySmallerVariable(int m, int b) 
 {
-    string nameMassiw = "";
-    int b, n, m;
-    cout << "Введите имя массива:";
-    cin >> nameMassiw;
-    cout << "Введите кол-во строк массива " << nameMassiw << ":";
-    cin >> n;
-    cout << "Введите кол-во столбцов массива " << nameMassiw << ":";
-    cin >> m;
-    cout << "Введите переменную от -50 до 50:" << endl;
-    cin >> b;
-    cout << endl;
-    cout << "Массив " << nameMassiw << ":" << endl;
-    int C[n][m];
+    int C[1000];
     int summ = 0;
     srand(time(0));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-         C[i][j] = rand() % 101 - 50;
-        }
-    } 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << C[i][j] << " ";
-            if (C[i][j] < b){
-                summ = summ + C[i][j];
-            }
-        }
-        cout << endl;
+    for (int i = 0; i < m; i++) {
+    C[i] = rand() % 101 - 50;
     }
-    cout << endl;
+    for (int i = 0; i < m; i++) {
+    if (C[i] < b){
+    summ = summ + C[i];
+    }
+    }
     return summ;
 }
-
-
 int main() 
 {
     int G, A;
-    G = sumValuesArreySmallerVariable();
+    G = sumValuesArreySmallerVariable(10 , 47);
     cout << G << endl;
-    A = sumValuesArreySmallerVariable();
+    A = sumValuesArreySmallerVariable(29, 15);
     cout << A << endl;
 }
